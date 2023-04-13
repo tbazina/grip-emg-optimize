472e5a5bcb097083e55210e99ea4a478791e9ebc
Tomislav <tbazina@gmail.com>
2023-04-13 14:50:39 +0200

    Working problem and test_GA.

--- problem.py
+++ problem.py
@@ -32,84 +32,86 @@ class SignalProcessingParams:
   """
   def __init__(self, group_number):
     self.group_number = group_number
-    self.emg_fft = None
-    self.fft_dim = None
+    self.emg_fft = []
+    self.grip_dat = []
     self.sampling_rate = None
     self.window_size = None
-    self.grip_dat = None
+    self.emg_fft_freq = None
+    self.fft_dim = None
     
     self.data_loader()
 
-
-  def data_loader(self, group_number):
+  def data_loader(self):
     time_dat = []
     emg_dat = []
     grip_dat = []
+    window_size_relative = 0.5                                     
+    # Smoothing dimension (decay factor for exponential smoothing + window size)
+    self.smooth_dim = 2
         
     directory = "new_data"
     pattern = os.path.join(directory, f"???{self.group_number}??_sl.csv")
             
     for filename in glob.glob(pattern):
       data = np.genfromtxt(filename, delimiter=",", skip_header=1)
+      # print(f'{data.shape=}')
+      # print(f'{filename=}')
       time_dat.append(data[:, 0])
       emg_dat.append(data[:, 1])
       grip_dat.append(data[:, 2])
       
-    # Combine data from multiple files into a single array
-    time_dat = np.concatenate(time_dat)
-    emg_dat = np.concatenate(emg_dat)
-    grip_dat = np.concatenate(grip_dat)
-    
-    # Data length
-    dat_len = time_dat.shape[0]
+    # print(f'{len(time_dat)=}')
+    # print(f'{len(emg_dat)=}')
+    # print(f'{len(grip_dat)=}')
+      
+    for time_t, emg, grip in zip(time_dat, emg_dat, grip_dat):
+      # Data length
+      dat_len = time_t.shape[0]
+      # print(f'{dat_len=}')
+      # Calculate sampling rate from time (remove first and last 10 data points) 
+      # Using median instead of mean - measurement imperfections
+      self.sampling_rate = 1. / np.median( np.diff(time_t[10:-10]))
+      # print(f'{self.sampling_rate=}')
+      # Calculate window size for FFT - always round to nearest even number
+      self.window_size = round(window_size_relative * self.sampling_rate)
+      self.window_size += (self.window_size % 2)
+      # print(f'{self.window_size=}')
+      # FFT resolution
+      self.fft_resolution = self.sampling_rate / self.window_size
+      # print(f'{self.fft_resolution=}')
 
-    # Calculate sampling rate from time (remove first and last 10 data points) 
-    # Using median instead of mean - measurement imperfections
-    self.sampling_rate = 1. / np.median( np.diff(time_dat[10:-10]))
+      # Reshape data to window_size × n array (disregard starting few points)
+      time_t = time_t[dat_len%self.window_size:].reshape((-1, self.window_size))
+      emg = emg[dat_len%self.window_size:].reshape((-1, self.window_size)) 
+      grip = grip[dat_len%self.window_size:].reshape((-1, self.window_size)) 
+      
+      # Calculate RFFT on emg_data
+      self.emg_fft.append(scipy.fft.rfft(emg))
+      # Store grip data
+      self.grip_dat.append(grip)
+      # Calculate RFFT frequencies and stack them
+      self.emg_fft_freq = scipy.fft.rfftfreq(n=self.window_size, d=1./self.sampling_rate)
+      # print(f'{self.emg_fft_freq=}')
+      # FFT problem dimension
+      self.fft_dim = self.emg_fft_freq.shape[0]
+      # Problem dimension
+      self.dim = self.fft_dim + self.smooth_dim
+      
+  def get_bounds(self):
+    #TODO: procjeriti ako uvijek ukloniti DC offset ili ne
+    # Calculate rolling window size for smoothing (must be <= FFT window size)
+    return (
+      [0.] * self.fft_dim + [0.] + [1], 
+      [2.] * self.fft_dim + [0.05] + [self.window_size-1]
+      )
 
-    # Calculate window size for FFT - always round to nearest even number
-    window_size_relative = 0.5                                     
-    self.window_size = round(window_size_relative * self.sampling_rate)
-    self.window_size += (self.window_size % 2)
-
-    # FFT resolution
-    fft_resolution = self.sampling_rate / self.window_size
-
-    # Reshape data to window_size × n array (disregard starting few points)
-    time_dat = time_dat[dat_len%self.window_size:].reshape((-1, self.window_size))
-    emg_dat = emg_dat[dat_len%self.window_size:].reshape((-1, self.window_size)) 
-    grip_dat = grip_dat[dat_len%self.window_size:].reshape((-1, self.window_size)) 
-    
-    # Calculate RFFT on emg_data
-    self.emg_fft = scipy.fft.rfft(emg_dat)
-
-    # Calculate RFFT frequencies and stack them
-    emg_fft_freq = scipy.fft.rfftfreq(n=self.window_size, d=1./self.sampling_rate)
-    # self.emg_fft_freq = np.vstack(
-    #   [self.emg_fft_freq for _ in range(self.dat_len//self.window_size)])
-    
-    # FFT problem dimension
-    self.fft_dim = emg_fft_freq.shape[0]
-
-    # Smoothing window dimension (size + decay factor for exponential smoothing)
-    smooth_dim = 2
-
-    # Problem dimension
-    self.dim = self.fft_dim + smooth_dim
-    
-    # Store grip data
-    self.grip_dat = grip_dat
-  
-  
-  def get_bounds(self) -> int:
-    return ([0.] * self.dim, [2.] * self.dim)
-
+  def get_nix(self) -> int:
+    return 1
 
   def get_name(self):
     return (
       'Optimize EMG signal processing parameters to cross-corelate with grip force'
       )
-
 
   def fitness(self, x):
     """Fitness function
@@ -117,57 +119,49 @@ class SignalProcessingParams:
     Args:
       x (np.float64): Decision vector
         [0:self.fft_dim] - Custom window for fft spectrum of signal (0-2)
-        [self.fft_dim] - Rolling window size for smoothing in sec (0-0.5)
+        [self.fft_dim] - Rolling window size for smoothing in sec (1-0.5)
         [self.fft_dim+1] - Exponential moving average decay (0-0.05)
         - amplification - maybe only in frequency domain 0 - 2 instead 0-1
     """
-    rolling_window_relative_size = [i/100 for i in range(1, 50)]
-    smoothing_factor = [i/1000 for i in range(50)]
     max_corrs = []
 
-    for params in itertools.product(rolling_window_relative_size, smoothing_factor):
-      
-      # Random 0 - 1, 250 kom
-      optimization_params = np.random.random(self.fft_dim)
-      optimization_params = np.append(optimization_params, params)
-      optimization_params[0] = 0 #zanemariti prvu vrijednost
-            
-      # Shallow copy of emg fft
-      emg_fft = np.copy(self.emg_fft)
-
-      # Filtering with custom filter
-      emg_fft = emg_fft * x[:self.fft_dim]
-
+    for emg_fft, grip_dat in zip(self.emg_fft, self.grip_dat):
+      # print(f'{emg_fft.shape=}')
       # Compute inverse FFT to reconstruct filtered signal as n-dim array
-      emg_ifft = scipy.fft.irfft(emg_fft * optimization_params[:self.fft_dim])
-
-      # Calculate rolling window size for smoothing (must be <= FFT window size)
-      rolling_window_size = round(optimization_params[self.fft_dim] * self.sampling_rate)
-      if rolling_window_size > self.window_size:
-        rolling_window_size  = self.window_size
-
-      # Construct 2-dimensional array using rolling window on iFFT
-      # emg_rolling = rolling_window_to_array(emg_ifft.ravel(), rolling_window_size)
+      emg_ifft = scipy.fft.irfft(emg_fft * x[:self.fft_dim])
+      # print(f'{emg_ifft.shape=}')
+      rolling_window_size = int(x[-1])
+      # print(f'{rolling_window_size=}')
       # Rectify flattened iFFT, construct window for smoothing and
       # calculate exponential moving average using FFT convolution
       emg_abs = np.abs(emg_ifft.ravel())
-      window_ema = np.array([(1-optimization_params[self.fft_dim+1])**i for i in range(rolling_window_size)]) 
+      # print(f'{emg_abs.shape=}')
+      window_ema = np.array([(1-x[-2])**i for i in range(rolling_window_size)]) 
       window_ema = window_ema / window_ema.sum()
+      # print(f'{window_ema.shape=}')
       emg_ema = scipy.signal.fftconvolve(emg_abs, window_ema[::-1], mode='valid')
+      # print(f'{emg_ema.shape=}')
       
-      grip_flat = self.grip_dat.ravel()[rolling_window_size-1:]
+      #TODO: što radi ovaj dio
+      grip_flat = grip_dat.ravel()[rolling_window_size-1:]
+      # print(f'{grip_flat.shape=}')
       emg_ema = emg_ema - emg_ema.mean()
       grip_flat = grip_flat - grip_flat.mean()
       
       corr_ema = scipy.signal.fftconvolve(emg_ema, grip_flat[::-1], mode='full')
+      # print(f'{corr_ema.shape=}')
       corr_ema /= (len(grip_flat) * emg_ema.std() * grip_flat.std())
+      # print(f'{corr_ema.shape=}')
       
       max_corr = np.abs(corr_ema).max()
+      # print(f'{max_corr=}')
       max_corrs.append(max_corr)
+      # print(f'{max_corrs=}')
     
     avg_corr = np.mean(max_corrs)
+    # print(f'{avg_corr=}')
       
-    return 1 - avg_corr
+    return [1-avg_corr]
   
   def get_extra_info(self):
     return (
